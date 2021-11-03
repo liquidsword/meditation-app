0f3a66a6f9a2aaee6f5ada5f426dccfdd03ab386
Auden Robertson <audenrobertson@gmail.com>
2021-11-03 19:55:24 -0400

    adding functionality to app document

---
+++ app.js
@@ -0,0 +1,42 @@
+const app = () => {
+    const song =  document.querySelector('.song');
+    const play = document.querySelector('.play');
+    const outline = document.querySelector('.moving-outline circle');
+    const video = document.querySelector('.vid-container video');
+
+    //Sounds
+
+    const sounds = document.querySelectorAll('.sound-picker button');
+
+    //Time Display
+    const timeDisplay = document.querySelector('.time-display');
+
+    //Get the length of the ouline
+    const outlineLength = outline.getTotalLength();
+    
+    //Duration
+    let standardDuration = 600;
+
+        outline.style.strokeDasharray = outlineLength;
+        outline.style.strokeDashoffset = outlineLength;
+
+    //Play Sound
+        play.addEventListener("click", ()=> {
+            song.play();
+        });
+
+    //Function specific to stop and play the sounds
+    const checkPlaying = song => {
+        if(song.paused){
+            song.play();
+            play.src = './svg/pause.svg';
+        } else {
+            song.pause();
+            video.pause();
+            play.src = "./svg/play.svg";
+        }
+    }
+}
+
+
+app();
